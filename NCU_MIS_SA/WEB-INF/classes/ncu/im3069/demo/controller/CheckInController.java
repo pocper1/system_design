84f57946b2b5ea646160d7b3b51ddbb2e52e3136
JohanesAlexander <95231241+JohanesAlexander@users.noreply.github.com>
2022-01-03 10:51:04 +0800

    fix bug

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/CheckInController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/CheckInController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ncu.im3069.demo.controller;
 
 import java.io.*;
@@ -32,6 +31,11 @@ public class CheckInController extends HttpServlet {
     /** mh，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
     private CheckInHelper ch =  CheckInHelper.getHelper();
     
+    public CheckInController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
     /**
      * 處理Http Method請求POST方法（新增資料）
      *
@@ -46,15 +50,23 @@ public class CheckInController extends HttpServlet {
         JsonReader jsr = new JsonReader(request);
         JSONObject jso = jsr.getObject();
         
-        boolean isDamaged=jso.getBoolean("isDamaged");
+        
         /** 取出經解析到JSONObject之Request參數 */
-        String dorm = jso.getString("dormNo");
-        String room = jso.getString("roomNo");
-        String bed = jso.getString("bedNo");
+        String name = jso.getString("name");
+        String userID = jso.getString("user_id");
+        String dorm = jso.getString("dorm_no");
+        String room = jso.getString("room_no");
+        String bed = jso.getString("bed_no");
+        String checkInStat = jso.getString("check_in_stat");
+        boolean isDamaged=jso.getBoolean("is_damaged");
 
         
         /** 建立一個新的會員物件 */
-        CheckIn c = new CheckIn(dorm, room, bed,isDamaged);
+        CheckIn c = new CheckIn(userID,name, dorm, room, bed,checkInStat,isDamaged);
+        
+        JSONObject result = ch.create(c);
+        System.out.println("response result: "+result);
+        //String userID, String name, String dorm, String roomNo, String bedNo, boolean isDamaged
         
         /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
         if(dorm.isEmpty() || room.isEmpty() || bed.isEmpty()) {
@@ -99,6 +111,8 @@ public class CheckInController extends HttpServlet {
         JsonReader jsr = new JsonReader(request);
         /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
         String id = jsr.getParameter("id");
+        String dormNo = jsr.getParameter("dorm_no");
+        String roomNo = jsr.getParameter("room_no");
         
         /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
         if (id.isEmpty()) {
@@ -116,6 +130,9 @@ public class CheckInController extends HttpServlet {
         else {
             /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
             JSONObject query = ch.getByID(id);
+            
+            
+            
             
             /** 新建一個JSONObject用於將回傳之資料進行封裝 */
             JSONObject resp = new JSONObject();
@@ -174,10 +191,12 @@ public class CheckInController extends HttpServlet {
         /** 取出經解析到JSONObject之Request參數 */
         int id = jso.getInt("id");
  
-        boolean isDamaged = jso.getBoolean("isDamaged");
+        boolean isDamaged = jso.getBoolean("is_damaged");
+        
+        String checkInStat = jso.getString("check_in_stat");
 
         /** 透過傳入之參數，新建一個以這些參數之會員Member物件 */
-        CheckIn c = new CheckIn(id, isDamaged);
+        CheckIn c = new CheckIn(id,checkInStat, isDamaged);
         
         /** 透過Member物件的update()方法至資料庫更新該名會員資料，回傳之資料為JSONObject物件 */
         JSONObject data = c.update();
@@ -191,198 +210,4 @@ public class CheckInController extends HttpServlet {
         jsr.response(resp, response);
     }
     
-=======
-package ncu.im3069.demo.controller;
-
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-import org.json.*;
-
-import ncu.im3069.demo.app.CheckIn;
-import ncu.im3069.demo.app.CheckInHelper;
-import ncu.im3069.tools.JsonReader;
-
-// TODO: Auto-generated Javadoc
-/**
- * <p>  
- * The Class MemberController<br>
- * MemberController類別（class）主要用於處理Member相關之Http請求（Request），繼承HttpServlet
- * </p>
- * 
- * @author IPLab
- * @version 1.0.0
- * @param <ch>
- * @since 1.0.0
- */
-@WebServlet("/api/check_in.do")
-public class CheckInController extends HttpServlet {
-    
-    /** The Constant serialVersionUID. */
-    private static final long serialVersionUID = 1L;
-    
-    /** mh，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
-    private CheckInHelper ch =  CheckInHelper.getHelper();
-    
-    /**
-     * 處理Http Method請求POST方法（新增資料）
-     *
-     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
-     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
-        JsonReader jsr = new JsonReader(request);
-        JSONObject jso = jsr.getObject();
-        
-        boolean isDamaged=jso.getBoolean("isDamaged");
-        /** 取出經解析到JSONObject之Request參數 */
-        String dorm = jso.getString("dormNo");
-        String room = jso.getString("roomNo");
-        String bed = jso.getString("bedNo");
-
-        
-        /** 建立一個新的會員物件 */
-        CheckIn c = new CheckIn(dorm, room, bed,isDamaged);
-        
-        /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
-        if(dorm.isEmpty() || room.isEmpty() || bed.isEmpty()) {
-            /** 以字串組出JSON格式之資料 */
-            String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
-            /** 透過JsonReader物件回傳到前端（以字串方式） */
-            jsr.response(resp, response);
-        }
-        /** 透過MemberHelper物件的checkDuplicate()檢查該會員電子郵件信箱是否有重複 */
-        else if (!ch.checkDuplicate(c)) {
-            /** 透過MemberHelper物件的create()方法新建一個會員至資料庫 */
-            JSONObject data = ch.create(c);
-            
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("message", "成功! 提交申請表...");
-            resp.put("response", data);
-            
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
-        }
-        else {
-            /** 以字串組出JSON格式之資料 */
-            String resp = "{\"status\": \'400\', \"message\": \'新增表單失敗，你曾填寫過申請表！\', \'response\': \'\'}";
-            /** 透過JsonReader物件回傳到前端（以字串方式） */
-            jsr.response(resp, response);
-        }
-    }
-
-    /**
-     * 處理Http Method請求GET方法（取得資料）
-     *
-     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
-     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
-        JsonReader jsr = new JsonReader(request);
-        /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
-        String id = jsr.getParameter("id");
-        
-        /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
-        if (id.isEmpty()) {
-            /** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
-            JSONObject query = ch.getAll();
-            
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("response", query);
-    
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
-        }
-        else {
-            /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
-            JSONObject query = ch.getByID(id);
-            
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("response", query);
-    
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
-        }
-    }
-
-    /**
-     * 處理Http Method請求DELETE方法（刪除）
-     *
-     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
-     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-    public void doDelete(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
-        JsonReader jsr = new JsonReader(request);
-        JSONObject jso = jsr.getObject();
-        
-        /** 取出經解析到JSONObject之Request參數 */
-        int id = jso.getInt("id");
-        
-        /** 透過MemberHelper物件的deleteByID()方法至資料庫刪除該名會員，回傳之資料為JSONObject物件 */
-        JSONObject query = ch.deleteByID(id);
-        
-        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-        JSONObject resp = new JSONObject();
-        resp.put("status", "200");
-        resp.put("message", "申請表移除成功！");
-        resp.put("response", query);
-
-        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-        jsr.response(resp, response);
-    }
-
-    /**
-     * 處理Http Method請求PUT方法（更新）
-     *
-     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
-     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-    public void doPut(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
-        JsonReader jsr = new JsonReader(request);
-        JSONObject jso = jsr.getObject();
-        
-        /** 取出經解析到JSONObject之Request參數 */
-        int id = jso.getInt("id");
- 
-        boolean isDamaged = jso.getBoolean("isDamaged");
-
-        /** 透過傳入之參數，新建一個以這些參數之會員Member物件 */
-        CheckIn c = new CheckIn(id, isDamaged);
-        
-        /** 透過Member物件的update()方法至資料庫更新該名會員資料，回傳之資料為JSONObject物件 */
-        JSONObject data = c.update();
-        
-        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-        JSONObject resp = new JSONObject();
-        resp.put("status", "200");
-        resp.put("response", data);
-        
-        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-        jsr.response(resp, response);
-    }
-    
->>>>>>> branch 'master' of https://github.com/pocper1/system_design
 }
