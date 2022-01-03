cc381100cfa892dd2760de562b4aeba86277d962
JohanesAlexander <95231241+JohanesAlexander@users.noreply.github.com>
2022-01-03 10:41:24 +0800

    Update CheckIn.java
    fix bug

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/CheckIn.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/CheckIn.java
@@ -19,6 +19,8 @@ public class CheckIn {
     
     private boolean isDamaged;
     
+    private String checkInStat;
+    
     /** create，訂單創建時間 */
     private Timestamp create;
 
@@ -92,45 +94,72 @@ public class CheckIn {
 		this.isDamaged = isDamaged;
 	}
 	
+	public String getCheckInStat() {
+		return checkInStat;
+	}
+
+	public void setCheckInStat(String checkInStat) {
+		this.checkInStat = checkInStat;
+	}
+
 
     
     
     
     private CheckInHelper ch =  CheckInHelper.getHelper();
     
-    public CheckIn(int id,String userID, String name, String dorm, String roomNo, String bedNo, boolean isDamaged, Timestamp created, Timestamp modified) {
+    public CheckIn(int id,String userID, String name, String dorm, String roomNo, String bedNo,String checkInStat,  boolean isDamaged, Timestamp created, Timestamp modified) {
     	this.id=id;
     	this.userID=userID;
         this.name=name;
         this.dorm = dorm;
+        this.roomNo=roomNo;
         this.bedNo = bedNo;
+        this.checkInStat=checkInStat;
         this.isDamaged = isDamaged;
         this.create = created;
         this.modify = Timestamp.valueOf(LocalDateTime.now());
         update();
     }
-
-    public CheckIn(String userID, String name, String dorm, String roomNo, String bedNo, boolean isDamaged, Timestamp created, Timestamp updated_at) {
+    
+  //String userID, String name, String dorm, String roomNo, String bedNo, boolean isDamaged
+    
+    public CheckIn(String userID, String name, String dorm, String roomNo, String bedNo, String checkInStat, boolean isDamaged) {
     	this.userID=userID;
         this.name=name;
         this.dorm = dorm;
+        this.roomNo=roomNo;
+        this.bedNo = bedNo;
+        this.checkInStat=checkInStat;
+        this.isDamaged = isDamaged;
+        update();
+    }
+
+    public CheckIn(String userID, String name, String dorm, String roomNo, String bedNo,String checkInStat, boolean isDamaged, Timestamp created, Timestamp updated_at) {
+    	this.userID=userID;
+        this.name=name;
+        this.dorm = dorm;
+        this.roomNo=roomNo;
         this.bedNo = bedNo;
         this.isDamaged = isDamaged;
-        this.create = created;
+        this.checkInStat= checkInStat;
+        this.create = Timestamp.valueOf(LocalDateTime.now());
         this.modify = Timestamp.valueOf(LocalDateTime.now());
         update();
     }
     
-    public CheckIn(int id,boolean isDamaged) {
+    public CheckIn(int id,String checkInStat, boolean isDamaged) {
     	this.id = id;
+    	this.checkInStat=checkInStat;
         this.isDamaged = isDamaged;
         update();
     }
     
-    public CheckIn(String dorm, String roomNo, String bedNo, boolean isDamaged) {
+    public CheckIn(String dorm, String roomNo, String bedNo,String checkInStat, boolean isDamaged) {
     	this.dorm=dorm;
     	this.roomNo=roomNo;
     	this.bedNo=bedNo;
+    	this.checkInStat=checkInStat;
     	this.isDamaged=isDamaged;
     	update();
     }
@@ -145,16 +174,20 @@ public class CheckIn {
     public JSONObject getData() {
 		JSONObject jso = new JSONObject();
 		jso.put("id", getId());
-		jso.put("userID", getUserID());
+		jso.put("user_id", getUserID());
 		jso.put("name", getName());
-		jso.put("dormNo", getDormNo());
-		jso.put("roomNo", getRoomNo());
-		jso.put("bedNo", getBedNo());
-		jso.put("isDamaged", getIsDamaged());
+		jso.put("dorm_no", getDormNo());
+		jso.put("room_no", getRoomNo());
+		jso.put("bed_no", getBedNo());
+		jso.put("check_in_stat", getCheckInStat());
+		jso.put("is_damaged", getIsDamaged());
+		jso.put("create_time", getCreateTime());
+		jso.put("modify_time", getModifyTime());
 
 		return jso;
     	
     }
+
 
 
 
