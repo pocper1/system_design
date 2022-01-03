7c54344d2d655ddc0f6ce48b507454c9fd27fe82
JohanesAlexander <95231241+JohanesAlexander@users.noreply.github.com>
2022-01-03 10:43:45 +0800

    fix bug

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/CheckInHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/CheckInHelper.java
@@ -22,7 +22,7 @@ public class CheckInHelper {
 	
 	
 	
-	public static CheckInHelper getHelper() {
+	public static CheckInHelper getHelper() { 
 		if(ch == null) ch = new CheckInHelper();
         
         return ch;
@@ -119,18 +119,20 @@ public class CheckInHelper {
                 
                 /** 將 ResultSet 之資料取出 */
                 int ci_id = rs.getInt("id");
-                String userID = rs.getString("userID");
+                String userID = rs.getString("user_id");
                 String name = rs.getString("name");
-                String dorm = rs.getString("dormNo");
-                String room = rs.getString("roomNo");
-                String bed = rs.getString("bedNo");
-                boolean isDamaged = rs.getBoolean("isDamaged");
-                Timestamp created = rs.getTimestamp("created");
-                Timestamp modified = rs.getTimestamp("modified");
+                String dorm = rs.getString("dorm_no");
+                String room = rs.getString("room_no");
+                String bed = rs.getString("bed_no");
+                String checkInStat = rs.getString("check_in_stat");
+                boolean isDamaged = rs.getBoolean("is_damaged");
+                Timestamp created = rs.getTimestamp("create_time");
+                Timestamp modified = rs.getTimestamp("modify_time");
                 
                 /** 將每一筆會員資料產生一名新Member物件 */
                 //int id,String userID, String name, String dorm, String roomNo, String bedNo, boolean isDamaged, Timestamp created, Timestamp modified
-                c = new CheckIn(ci_id,userID, name, dorm, room, bed,isDamaged, created, modified);
+                c = new CheckIn(ci_id,userID, name, dorm, room, bed,checkInStat,isDamaged, created, modified);
+                //int id,String userID, String name, String dorm, String roomNo, String bedNo,String checkInStat,  boolean isDamaged, Timestamp created, Timestamp modified
                 /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
                 jsa.put(c.getData());
             }
@@ -160,6 +162,9 @@ public class CheckInHelper {
 
         return response;
     }
+    
+    
+    
     
     
     public JSONObject getByID(String id) {
@@ -199,18 +204,102 @@ public class CheckInHelper {
                 //row += 1;
                 
                 /** 將 ResultSet 之資料取出 */
+
                 int ci_id = rs.getInt("id");
-                String userID = rs.getString("userID");
+                String userID = rs.getString("user_id");
                 String name = rs.getString("name");
-                String dorm = rs.getString("dormNo");
-                String room = rs.getString("roomNo");
-                String bed = rs.getString("bedNo");
-                boolean isDamaged = rs.getBoolean("isDamaged");
-                Timestamp created = rs.getTimestamp("created");
-                Timestamp modified = rs.getTimestamp("modified");
+                String dorm = rs.getString("dorm_no");
+                String room = rs.getString("room_no");
+                String bed = rs.getString("bed_no");
+                String checkInStat = rs.getString("check_in_stat");
+                boolean isDamaged = rs.getBoolean("is_damaged");
+                Timestamp created = rs.getTimestamp("create_time");
+                Timestamp modified = rs.getTimestamp("modify_time");
                 
                 /** 將每一筆會員資料產生一名新Member物件 */
-                c = new CheckIn(ci_id,userID, name, dorm, room, bed,isDamaged, created, modified);
+                c = new CheckIn(ci_id,userID, name, dorm, room, bed,checkInStat,isDamaged, created, modified);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                jsa.put(c.getData());
+            }
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    
+    public JSONObject getByRoom(String dormNo, String roomNo) {
+        /** 新建一個 Member 物件之 m 變數，用於紀錄每一位查詢回之會員資料 */
+        CheckIn c = null;
+        /** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `missa`.`check_in` WHERE `dorm_no`=?, `room_no` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, dormNo);
+            pres.setString(2, roomNo);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            /** 正確來說資料庫只會有一筆該會員編號之資料，因此其實可以不用使用 while 迴圈 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                //row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+
+                int ci_id = rs.getInt("id");
+                String userID = rs.getString("user_id");
+                String name = rs.getString("name");
+                String dorm = rs.getString("dorm_no");
+                String room = rs.getString("room_no");
+                String bed = rs.getString("bed_no");
+                String checkInStat = rs.getString("check_in_stat");
+                boolean isDamaged = rs.getBoolean("is_damaged");
+                Timestamp created = rs.getTimestamp("create_time");
+                Timestamp modified = rs.getTimestamp("modify_time");
+                
+                /** 將每一筆會員資料產生一名新Member物件 */
+                c = new CheckIn(ci_id,userID, name, dorm, room, bed,checkInStat,isDamaged, created, modified);
                 /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
                 jsa.put(c.getData());
             }
@@ -259,6 +348,7 @@ public class CheckInHelper {
             String dorm = c.getDormNo();
             String room = c.getRoomNo();
             String bed = c.getBedNo();
+            System.out.println(dorm + room + bed);
             
             /** 將參數回填至SQL指令當中 */
             pres = conn.prepareStatement(sql);
@@ -307,8 +397,8 @@ public class CheckInHelper {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "INSERT INTO `missa`.`check_in`(`user_id`, `name`,`dorm_no`, `room_no`, `bed_no`,`is_damaged`, `create_time` ,`modify_time`)"
-                    + " VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO `missa`.`check_in`(`user_id`, `name`,`dorm_no`, `room_no`, `bed_no`,`check_in_stat`,`is_damaged`, `create_time` ,`modify_time`)"
+                    + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
             
             /** 取得所需之參數 */
             String userID = c.getUserID();
@@ -316,6 +406,7 @@ public class CheckInHelper {
             String dorm = c.getDormNo();
             String room = c.getRoomNo();
             String bed = c.getBedNo();
+            String checkInStat = c.getCheckInStat();
             boolean isDamaged = c.getIsDamaged();
             Timestamp create = c.getCreateTime();
             Timestamp modify = c.getModifyTime();
@@ -327,9 +418,10 @@ public class CheckInHelper {
             pres.setString(3, dorm);
             pres.setString(4, room);
             pres.setString(5, bed);
-            pres.setBoolean(6, isDamaged);
-            pres.setTimestamp(7, create);
-            pres.setTimestamp(12, modify);
+            pres.setString(6,checkInStat);
+            pres.setBoolean(7, isDamaged);
+            pres.setTimestamp(8, create);
+            pres.setTimestamp(9, modify);
             
 
             
@@ -387,15 +479,18 @@ public class CheckInHelper {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "Update `missa`.`check_in` SET `passORnot` = ? WHERE `id` = ?";
+            String sql = "Update `missa`.`check_in` SET `is_damaged` = ?, `check_in_stat` = ? WHERE `id` = ?";
             /** 取得所需之參數 */
             int id = c.getId();
             boolean isDamaged = c.getIsDamaged();
+            String checkInStat = c.getCheckInStat();
+            
             
             /** 將參數回填至SQL指令當中 */
             pres = conn.prepareStatement(sql);
             pres.setBoolean(1, isDamaged);
-            pres.setInt(2, id);
+            pres.setString(2, checkInStat);
+            pres.setInt(3, id);
             /** 執行更新之SQL指令並記錄影響之行數 */
             row = pres.executeUpdate();
 
