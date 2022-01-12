ea7e3aca87cf20a3177c6c728490c01ca4a6fc7d
Jimmy Huang <pocper1@gmail.com>
2022-01-12 20:18:19 +0800

    reset to past add back html

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/CommentController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/CommentController.java
@@ -12,24 +12,25 @@ import org.json.JSONObject;
 
 import ncu.im3069.tools.JsonReader;
 
-import ncu.im3069.demo.app.DiscussionHelper;
+import ncu.im3069.demo.app.CommentHelper;
+import ncu.im3069.demo.app.Comment;
 import ncu.im3069.demo.app.Discussion;
 
 /**
  * Servlet implementation class DiscussionController
  */
-@WebServlet("/api/discussion.do")
-public class DiscussionController extends HttpServlet {
+@WebServlet("/api/comment.do")
+public class CommentController extends HttpServlet {
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
        
 	/** ph，DiscussionHelper 之物件與 Discussion 相關之資料庫方法（Sigleton） */
-	private DiscussionHelper ph =  DiscussionHelper.getHelper();
+	private CommentHelper ph =  CommentHelper.getHelper();
 	
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DiscussionController() {
+    public CommentController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -60,20 +61,23 @@ public class DiscussionController extends HttpServlet {
         /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
         if (id.isEmpty()) {
             /** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
-            JSONObject query = ph.getAll();
+        	System.out.println(id);
+        	JSONObject query = ph.getAll(id);
             
             /** 新建一個JSONObject用於將回傳之資料進行封裝 */
             JSONObject resp = new JSONObject();
             resp.put("status", "200");
-            resp.put("message", "所有討論資料取得成功");
+            resp.put("message", "所有留言資料取得成功");
             resp.put("response", query);
     
             /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
             jsr.response(resp, response);
         }
         else {
+        	System.out.println(id);
+        	System.out.println("fuck");
             /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
-            JSONObject query = ph.getByID(id);
+            JSONObject query = ph.getById(id);
             
             /** 新建一個JSONObject用於將回傳之資料進行封裝 */
             JSONObject resp = new JSONObject();
@@ -96,53 +100,52 @@ public class DiscussionController extends HttpServlet {
 	        JSONObject jso = jsr.getObject();
 	
 	        /** 取出經解析到 JSONObject 之 Request 參數 */
-
-	        String article_title 	= jso.getString("article_title");
-	        String article_content 	= jso.getString("article_content");
-	        int author 				= jso.getInt("author");
+	        String discussion_id 		= jso.getString("discussion_id");
+	        int user_id 				= jso.getInt("user_id");
+	        String content 				= jso.getString("content");
 	       
 	        /** 建立一個新的討論物件 */
-	        Discussion dis = new Discussion(article_title, article_content, author);
+	        Comment dis = new Comment(discussion_id, user_id, content);
 
 	   
-	        /** 透過 DiscussionHelper 物件的 create() 方法新建一筆訂單至資料庫 */
+	        /** 透過 CommentHelper 物件的 create() 方法新建一筆訂單至資料庫 */
 	        JSONObject result = ph.create(dis);
 	        System.out.println("this is response:"+ result);
 	        /** 設定回傳回來的訂單編號與訂單細項編號 */
-	        dis.setID((int) result.getLong("article_id"));
-	        // od.setOrderProductId(result.getJSONArray("order_product_id"));
+	        dis.setID((int) result.getLong("id"));
+	       
 	        
 
 	        /** 新建一個 JSONObject 用於將回傳之資料進行封裝 */
 	        JSONObject resp = new JSONObject();
 	        resp.put("status", "200");
 	        resp.put("message", "訂單新增成功！");
-	        resp.put("response", ph.getAll());
+	        resp.put("response", ph.getAll(discussion_id));
 
 	        /** 透過 JsonReader 物件回傳到前端（以 JSONObject 方式） */
 	        jsr.response(resp, response);
 	}
 
-	 public void doDelete(HttpServletRequest request, HttpServletResponse response)
-		        throws ServletException, IOException {
-		 		
-		        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
-		        JsonReader jsr = new JsonReader(request);
-		        JSONObject jso = jsr.getObject();
-		        
-		        /** 取出經解析到JSONObject之Request參數 */
-		        int id = jso.getInt("id");
-		        
-		        /** 透過MemberHelper物件的deleteByID()方法至資料庫刪除該名會員，回傳之資料為JSONObject物件 */
-		        JSONObject query = ph.deleteByID(id);
-		        
-		        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-		        JSONObject resp = new JSONObject();
-		        resp.put("status", "200");
-		        resp.put("message", "移除文章成功！");
-		        resp.put("response", query);
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+			System.out.println("testing \n");
+	        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+	        JsonReader jsr = new JsonReader(request);
+	        JSONObject jso = jsr.getObject();
+	        
+	        /** 取出經解析到JSONObject之Request參數 */
+	        int id = jso.getInt("id");
+	        
+	        /** 透過CommentHelper物件的deleteByID()方法至資料庫刪除該篇文章，回傳之資料為JSONObject物件 */
+	        JSONObject query = ph.deleteByID(id);
+	        
+	        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+	        JSONObject resp = new JSONObject();
+	        resp.put("status", "200");
+	        resp.put("message", "移除留言成功！");
+	        resp.put("response", query);
 
-		        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-		        jsr.response(resp, response);
-		    }
+	        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+	        jsr.response(resp, response);
+	    }
 }
