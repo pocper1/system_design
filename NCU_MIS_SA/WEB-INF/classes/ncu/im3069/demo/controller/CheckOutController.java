52af121bbe882017fcc803082fdccd11e1e43c25
User <User@User>
2022-01-14 02:16:38 +0800

    fix CheckOutController

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/CheckOutController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/CheckOutController.java
@@ -13,7 +13,7 @@ import ncu.im3069.tools.JsonReader;
 /**
  * <p>
  * The Class MemberController<br>
- * MemberController���O�]class�^�D�n�Ω�B�zMember������Http�ШD�]Request�^�A�~��HttpServlet
+ * MemberController嚙踝蕭嚙瞌嚙稽class嚙稷嚙瘩嚙緯嚙諄抬蕭B嚙緲Member嚙踝蕭嚙踝蕭嚙踝蕭Http嚙請求嚙稽Request嚙稷嚙璀嚙羯嚙踝蕭HttpServlet
  * </p>
  * 
  * @author IPLab
@@ -26,134 +26,134 @@ public class CheckOutController extends HttpServlet {
     /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
     
-    /** mh�AMemberHelper������PMember��������Ʈw��k�]Sigleton�^ */
+    /** mh嚙璀MemberHelper嚙踝蕭嚙踝蕭嚙踝蕭PMember嚙踝蕭嚙踝蕭嚙踝蕭嚙踝蕭w嚙踝蕭k嚙稽Sigleton嚙稷 */
     private CheckOutHelper ch =  CheckOutHelper.getHelper();
     
     /**
-     * �B�zHttp Method�ШDPOST��k�]�s�W��ơ^
+     * 嚙畿嚙緲Http Method嚙請求POST嚙踝蕭k嚙稽嚙編嚙磕嚙踝蕭^
      *
-     * @param request Servlet�ШD��HttpServletRequest��Request����]�e�ݨ��ݡ^
-     * @param response Servlet�^�Ǥ�HttpServletResponse��Response����]��ݨ�e�ݡ^
+     * @param request Servlet嚙請求嚙踝蕭HttpServletRequest嚙踝蕭Request嚙踝蕭嚙踝蕭]嚙箴嚙豎剁蕭嚙豎）
+     * @param response Servlet嚙稷嚙褒歹蕭HttpServletResponse嚙踝蕭Response嚙踝蕭嚙踝蕭]嚙踝蕭搢嚙箴嚙豎）
      * @throws ServletException the servlet exception
      * @throws IOException Signals that an I/O exception has occurred.
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        /** �z�LJsonReader���O�NRequest��JSON�榡��ƸѪR�è��^ */
+        /** 嚙緲嚙盤JsonReader嚙踝蕭嚙瞌嚙瞇Request嚙踝蕭JSON嚙賣式嚙踝蕭悛R嚙衛剁蕭嚙稷 */
         JsonReader jsr = new JsonReader(request);
         JSONObject jso = jsr.getObject();
         
         String passORnot="NotPass";
-        /** ���X�g�ѪR��JSONObject��Request�Ѽ� */
+        /** 嚙踝蕭嚙碼嚙篇嚙諸析嚙踝蕭JSONObject嚙踝蕭Request嚙諸潘蕭 */
         String name = jso.getString("UserId");
         String dorm = jso.getString("dorm");
         String room = jso.getString("room");
         String bed = jso.getString("bed");
 
         
-        /** �إߤ@�ӷs���|������ */
+        /** 嚙諍立一嚙諉新嚙踝蕭嚙罵嚙踝蕭嚙踝蕭嚙踝蕭 */
         CheckOut c = new CheckOut(name, dorm, room, bed, passORnot);
         
-        /** ����ˬd�O�_����쬰�ŭȡA�Y���h�^�ǿ��~�T�� */
+        /** 嚙踝蕭嚙踝蕭邠d嚙瞌嚙稻嚙踝蕭嚙踝蕭鴐堆蕭躑A嚙磐嚙踝蕭嚙篁嚙稷嚙褒選蕭嚙羯嚙確嚙踝蕭 */
         JSONObject resp = new JSONObject();
         if(!ch.checkDuplicate(c)) {
         	ch.create(c);
-        	resp.put("status", "250");}
+        	resp.put("status", "200");}
         else {resp.put("status", "250");}
          
          
-            /** �s�ؤ@��JSONObject�Ω�N�^�Ǥ���ƶi��ʸ� */
+            /** 嚙編嚙諍一嚙踝蕭JSONObject嚙諄抬蕭N嚙稷嚙褒歹蕭嚙踝蕭i嚙踝蕭妐嚙� */
 
          
             
-            /** �z�LJsonReader����^�Ǩ�e�ݡ]�HJSONObject�覡�^ */
+            /** 嚙緲嚙盤JsonReader嚙踝蕭嚙踝蕭^嚙褒剁蕭e嚙豎（嚙瘡JSONObject嚙質式嚙稷 */
          jsr.response(resp, response);
         
 
     }
 
     /**
-     * �B�zHttp Method�ШDGET��k�]���o��ơ^
+     * 嚙畿嚙緲Http Method嚙請求GET嚙踝蕭k嚙稽嚙踝蕭嚙緻嚙踝蕭^
      *
-     * @param request Servlet�ШD��HttpServletRequest��Request����]�e�ݨ��ݡ^
-     * @param response Servlet�^�Ǥ�HttpServletResponse��Response����]��ݨ�e�ݡ^
+     * @param request Servlet嚙請求嚙踝蕭HttpServletRequest嚙踝蕭Request嚙踝蕭嚙踝蕭]嚙箴嚙豎剁蕭嚙豎）
+     * @param response Servlet嚙稷嚙褒歹蕭HttpServletResponse嚙踝蕭Response嚙踝蕭嚙踝蕭]嚙踝蕭搢嚙箴嚙豎）
      * @throws ServletException the servlet exception
      * @throws IOException Signals that an I/O exception has occurred.
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        /** �z�LJsonReader���O�NRequest��JSON�榡��ƸѪR�è��^ */
+        /** 嚙緲嚙盤JsonReader嚙踝蕭嚙瞌嚙瞇Request嚙踝蕭JSON嚙賣式嚙踝蕭悛R嚙衛剁蕭嚙稷 */
         JsonReader jsr = new JsonReader(request);
-        /** �Y�����z�L�e��AJAX��data�Hkey=value���r��覡�i��ǻ��ѼơA�i�H�����Ѧ���k���^��� */
+        /** 嚙磐嚙踝蕭嚙踝蕭嚙緲嚙盤嚙箴嚙踝蕭AJAX嚙踝蕭data嚙瘡key=value嚙踝蕭嚙緝嚙踝蕭閬∴蕭i嚙踝蕭嚙踝蕭捊A嚙箠嚙瘡嚙踝蕭嚙踝蕭嚙諸佗蕭嚙踝蕭k嚙踝蕭嚙稷嚙踝蕭嚙� */
         String name = jsr.getParameter("UserId");
         System.out.println(name);
-        /** �P�_�Ӧr��O�_�s�b�A�Y�s�b�N��n���^�ӧO�|������ơA�_�h�N��n���^������Ʈw���|������� */
+        /** 嚙瞑嚙稻嚙諉字嚙踝蕭O嚙稻嚙編嚙箭嚙璀嚙磐嚙編嚙箭嚙瞇嚙踝蕭n嚙踝蕭嚙稷嚙諉別嚙罵嚙踝蕭嚙踝蕭嚙踝蕭A嚙稻嚙篁嚙瞇嚙踝蕭n嚙踝蕭嚙稷嚙踝蕭嚙踝蕭嚙踝蕭w嚙踝蕭嚙罵嚙踝蕭嚙踝蕭嚙踝蕭嚙� */
         if (name.isEmpty()) {
-            /** �z�LMemberHelper����getAll()��k���^�Ҧ��|������ơA�^�Ǥ���Ƭ�JSONObject���� */
+            /** 嚙緲嚙盤MemberHelper嚙踝蕭嚙踝蕭getAll()嚙踝蕭k嚙踝蕭嚙稷嚙課佗蕭嚙罵嚙踝蕭嚙踝蕭嚙踝蕭A嚙稷嚙褒歹蕭嚙踝蕭嚙皚SONObject嚙踝蕭嚙踝蕭 */
             JSONObject query = ch.getAll();
             System.out.println("2");
-            /** �s�ؤ@��JSONObject�Ω�N�^�Ǥ���ƶi��ʸ� */
+            /** 嚙編嚙諍一嚙踝蕭JSONObject嚙諄抬蕭N嚙稷嚙褒歹蕭嚙踝蕭i嚙踝蕭妐嚙� */
             JSONObject resp = new JSONObject();
             resp.put("status", "200");
             resp.put("response", query);
-            /** �z�LJsonReader����^�Ǩ�e�ݡ]�HJSONObject�覡�^ */
+            /** 嚙緲嚙盤JsonReader嚙踝蕭嚙踝蕭^嚙褒剁蕭e嚙豎（嚙瘡JSONObject嚙質式嚙稷 */
             jsr.response(resp, response);
         }
         else {
-            /** �z�LMemberHelper����getByID()��k�۸�Ʈw���^�ӦW�|������ơA�^�Ǥ���Ƭ�JSONObject���� */
+            /** 嚙緲嚙盤MemberHelper嚙踝蕭嚙踝蕭getByID()嚙踝蕭k嚙諛賂蕭w嚙踝蕭嚙稷嚙諉名嚙罵嚙踝蕭嚙踝蕭嚙踝蕭A嚙稷嚙褒歹蕭嚙踝蕭嚙皚SONObject嚙踝蕭嚙踝蕭 */
             JSONObject query = ch.getByname(name);
-            /** �s�ؤ@��JSONObject�Ω�N�^�Ǥ���ƶi��ʸ� */
+            /** 嚙編嚙諍一嚙踝蕭JSONObject嚙諄抬蕭N嚙稷嚙褒歹蕭嚙踝蕭i嚙踝蕭妐嚙� */
             JSONObject resp = new JSONObject();
             resp.put("status", "200");
             resp.put("response", query);
             System.out.println(query);
             System.out.println("3");
-            /** �z�LJsonReader����^�Ǩ�e�ݡ]�HJSONObject�覡�^ */
+            /** 嚙緲嚙盤JsonReader嚙踝蕭嚙踝蕭^嚙褒剁蕭e嚙豎（嚙瘡JSONObject嚙質式嚙稷 */
             jsr.response(resp, response);
         }
     }
 
     /**
-     * �B�zHttp Method�ШDDELETE��k�]�R���^
+     * 嚙畿嚙緲Http Method嚙請求DELETE嚙踝蕭k嚙稽嚙磋嚙踝蕭嚙稷
      *
-     * @param request Servlet�ШD��HttpServletRequest��Request����]�e�ݨ��ݡ^
-     * @param response Servlet�^�Ǥ�HttpServletResponse��Response����]��ݨ�e�ݡ^
+     * @param request Servlet嚙請求嚙踝蕭HttpServletRequest嚙踝蕭Request嚙踝蕭嚙踝蕭]嚙箴嚙豎剁蕭嚙豎）
+     * @param response Servlet嚙稷嚙褒歹蕭HttpServletResponse嚙踝蕭Response嚙踝蕭嚙踝蕭]嚙踝蕭搢嚙箴嚙豎）
      * @throws ServletException the servlet exception
      * @throws IOException Signals that an I/O exception has occurred.
      */
 
 
     /**
-     * �B�zHttp Method�ШDPUT��k�]��s�^
+     * 嚙畿嚙緲Http Method嚙請求PUT嚙踝蕭k嚙稽嚙踝蕭s嚙稷
      *
-     * @param request Servlet�ШD��HttpServletRequest��Request����]�e�ݨ��ݡ^
-     * @param response Servlet�^�Ǥ�HttpServletResponse��Response����]��ݨ�e�ݡ^
+     * @param request Servlet嚙請求嚙踝蕭HttpServletRequest嚙踝蕭Request嚙踝蕭嚙踝蕭]嚙箴嚙豎剁蕭嚙豎）
+     * @param response Servlet嚙稷嚙褒歹蕭HttpServletResponse嚙踝蕭Response嚙踝蕭嚙踝蕭]嚙踝蕭搢嚙箴嚙豎）
      * @throws ServletException the servlet exception
      * @throws IOException Signals that an I/O exception has occurred.
      */
     public void doPut(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        /** �z�LJsonReader���O�NRequest��JSON�榡��ƸѪR�è��^ */
+        /** 嚙緲嚙盤JsonReader嚙踝蕭嚙瞌嚙瞇Request嚙踝蕭JSON嚙賣式嚙踝蕭悛R嚙衛剁蕭嚙稷 */
         JsonReader jsr = new JsonReader(request);
         JSONObject jso = jsr.getObject();
         
-        /** ���X�g�ѪR��JSONObject��Request�Ѽ� */
+        /** 嚙踝蕭嚙碼嚙篇嚙諸析嚙踝蕭JSONObject嚙踝蕭Request嚙諸潘蕭 */
         int id = jso.getInt("id");
  
         String passORnot = "Pass";
 
-        /** �z�L�ǤJ���ѼơA�s�ؤ@�ӥH�o�ǰѼƤ��|��Member���� */
+        /** 嚙緲嚙盤嚙褒入嚙踝蕭嚙諸數，嚙編嚙諍一嚙諉以嚙緻嚙褒參數歹蕭嚙罵嚙踝蕭Member嚙踝蕭嚙踝蕭 */
         CheckOut c = new CheckOut(id, passORnot);
         
-        /** �z�LMember����update()��k�ܸ�Ʈw��s�ӦW�|����ơA�^�Ǥ���Ƭ�JSONObject���� */
+        /** 嚙緲嚙盤Member嚙踝蕭嚙踝蕭update()嚙踝蕭k嚙豌賂蕭w嚙踝蕭s嚙諉名嚙罵嚙踝蕭嚙踝蕭A嚙稷嚙褒歹蕭嚙踝蕭嚙皚SONObject嚙踝蕭嚙踝蕭 */
         JSONObject data = c.update();
         
-        /** �s�ؤ@��JSONObject�Ω�N�^�Ǥ���ƶi��ʸ� */
+        /** 嚙編嚙諍一嚙踝蕭JSONObject嚙諄抬蕭N嚙稷嚙褒歹蕭嚙踝蕭i嚙踝蕭妐嚙� */
         JSONObject resp = new JSONObject();
         resp.put("status", "200");
         resp.put("response", data);
         
-        /** �z�LJsonReader����^�Ǩ�e�ݡ]�HJSONObject�覡�^ */
+        /** 嚙緲嚙盤JsonReader嚙踝蕭嚙踝蕭^嚙褒剁蕭e嚙豎（嚙瘡JSONObject嚙質式嚙稷 */
         jsr.response(resp, response);
     }
     
